322bc96fe9f162fca5cb11b79fddc4df28e57064
Gaëtan Haefeli <gaetan.hafeli@heig-vd.ch>
2018-10-24 19:00:22 +0200

    add function toggle

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ switch($_POST['action']) {
 
     $id = $_POST['id'];
     if(is_numeric($id)) {
-      $updateQuery = ''; // IMPLEMENT ME
+      $updateQuery = 'update todo set done = 0 where id ='.$id; // IMPLEMENT ME
       if(!$db->query($updateQuery)) {
         die(print_r($db->errorInfo(), true));
       }
