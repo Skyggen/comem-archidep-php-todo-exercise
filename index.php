87531256ddc9de3702e5f79278fd064b578a9de3
Adrien Ciampone <39824607+Skyggen@users.noreply.github.com>
2019-01-11 15:50:58 +0100

    update title

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ $items = $db->query($selectQuery);
       <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <strong>TodoList</strong>
+            <strong>TodoList 2019</strong>
           </a>
         </div>
       </div>
